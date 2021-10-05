c565a60a3363b84e7c0ce642b07d20bde97c14fb
Yashwanth <yashvanthyashujnvt@gmail.com>
2021-10-05 13:46:10 +0530

    user category commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
+
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
